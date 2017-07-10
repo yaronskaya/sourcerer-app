ba6a903606afd0890a8ad8fbb81c779334c43627
Anatoly Kislov <anatoly@sourcerer.io>
2017-07-10 23:13:02 +0300

    Set up jar assembly with dependencies. Added number of added and deleted lines to Commit message because aggregated number from Stats may be not accurate. Entering data into the console.

--- src/main/kotlin/app/Greeter.kt
+++ src/main/kotlin/app/Greeter.kt
@@ -2,8 +2,8 @@
 
 package app
 
-import app.utils.Args
-import app.utils.EmailValidator
+import app.utils.*
+import java.io.IOError
 
 class Greeter(args: Args) {
     var username: String? = args.username  // Sourcerer account username.
@@ -13,12 +13,73 @@ class Greeter(args: Args) {
 
     fun run() {
         if (!silent) {
-            println("Sourcerer app")
+            println("Sourcerer app. More info at http://sourcerer.io")
         }
 
         if (username == null) {
             println("Enter your Sourcerer username:")
-            username = readLine()
+            var usernameTemp = readLine()
+
+            while (usernameTemp == null
+                    || UsernameValidator.isValidUsername(usernameTemp)) {
+                println("Enter correct Sourcerer username "
+                        + "(use only latin alphabet, numbers, underscore, "
+                        + "plus, minus or dot).")
+                usernameTemp = readLine()
+            }
+
+            username = usernameTemp
+        }
+
+        if (password == null) {
+            var passwordTemp: String?
+            println("Enter your Sourcerer password:")
+
+            // Check for the console is necessary because when running from IDE
+            // console is simulated using in, out and err streams and
+            // application aren't associated with any terminal/console.
+            val console = System.console()
+            if (console != null) {
+                try {
+                    passwordTemp = System.console().readPassword()
+                            .joinToString()
+                    while (passwordTemp == null
+                            || PasswordValidator
+                            .isValidPassword(passwordTemp)) {
+                        println("Enter correct Sourcerer password.")
+                        passwordTemp = System.console().readPassword()
+                                .joinToString()
+                    }
+                } catch (e: IOError) {
+                    passwordTemp = ""
+                }
+            } else {
+                passwordTemp = readLine()
+
+                while (passwordTemp == null
+                        || PasswordValidator.isValidPassword(passwordTemp)) {
+                    println("Enter correct Sourcerer password.")
+                    passwordTemp = readLine()
+                }
+            }
+
+            password = passwordTemp
+        }
+
+        // TODO(anatoly): Implement authorization check.
+
+        if (path == null) {
+            println("Enter path to the folder that contains "
+                    + "your local repository")
+            var pathTemp = readLine()
+
+            while (pathTemp == null
+                    || PathValidator.isValidPath(pathTemp)) {
+                println("Enter correct path to the folder with repository.")
+                pathTemp = readLine()
+            }
+
+            path = pathTemp
         }
     }
 }
